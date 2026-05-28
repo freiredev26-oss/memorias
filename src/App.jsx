72b32186b18764a71dd35e02c41fa1ad4b5f378e
MauroDev2013 <mauro2013oliv@gmail.com>
2026-05-28 08:52:38 -0400

    [FIX]Recolocando a NavBar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom"
+import NavBar from "../src/components/navbar/Navbar"
 import Home from "./pages/Home"
 import './App.css'
 
 export default function App() {
   return(
       <BrowserRouter>
+      <NavBar />
       <Home />
       </BrowserRouter>
   )
